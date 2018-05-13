c71965918d617cafd032186c8cba84a872f578b3
alongigi <giga@post.bgu.ac.il>
2018-05-13 12:33:31 +0300

    Initial commit

---
+++ src/View/IView.java
@@ -0,0 +1,14 @@
+package View;
+
+import javafx.scene.input.KeyCode;
+
+/**
+ * Created by alongigi on 6/19/2017.
+ */
+public interface IView {
+
+    void handleReacheGoal();
+
+    void displayMaze(int[][] maze);
+
+}
